255bc205c5a33613263d36567d643546d7174485
ZeNiXxX <vitjahermann@gmail.com>
2018-12-21 02:35:03 +0530

    Adapt slim recents to our recents style

--- src/com/artstar/settings/fragments/RecentsSettings.java
+++ src/com/artstar/settings/fragments/RecentsSettings.java
@@ -4,16 +4,18 @@ import com.android.internal.logging.nano.MetricsProto;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.provider.Settings;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
-import android.provider.Settings;
 import com.android.settings.R;
 
 import java.util.Arrays;
@@ -24,6 +26,10 @@ import com.android.settings.SettingsPreferenceFragment;
 public class RecentsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+	private static final String KEY_CATEGORY_SLIM = "slim_recents_category";
+
+	private PreferenceCategory mSlimCat;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -32,6 +38,41 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
 
+	mSlimCat = (PreferenceCategory) findPreference(KEY_CATEGORY_SLIM);
+        updateRecentsState(type);
+
+    }
+
+    public void updateRecentsState(int type) {
+        switch(type) {
+            case 0:
+                mSlimCat.setEnabled(false);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 0);
+                break;
+            case 1:
+                mSlimCat.setEnabled(false);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 0);
+                break;
+            case 2:
+                mSlimCat.setEnabled(false);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 0);
+                break;
+            case 3:
+                mSlimCat.setEnabled(false);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 0);
+                break;
+            case 4:
+                mSlimCat.setEnabled(true);
+                Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.USE_SLIM_RECENTS, 1);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
